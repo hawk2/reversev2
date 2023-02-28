15910584bf6fa9136a0ffeb0d7124db2fc96e5c8
MatthewCraig <mcraig@bizlibrary.com>
2023-02-27 23:51:07 -0800

    added some reverse stuff

---
+++ main.js
@@ -0,0 +1,27 @@
+//built js function
+function reverse(arg) {
+    return arg.split("").reverse().join("");
+}
+
+
+//make some other thingys that reverse some strings
+function reverseV2(something){
+    //something here
+}
+
+$(document).ready(function() {
+   $("#clear").click(function(event){
+    $("#results").empty();
+    $("#talky").empty();
+    event.preventDefault();
+   })
+})
+$(document).ready(function() {
+    $("#reverse").submit(function(event){
+        $("#results").empty();
+        let entry = $("input#talky").val();
+        $("#results").append(reverse(entry));
+
+        event.preventDefault();
+    })
+})
